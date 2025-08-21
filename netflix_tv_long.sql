afc1d3516e7c6500741cb5c4feafb4284a009758
dannihuang178-star <dannihuang178@gmail.com>
2025-08-21 15:32:14 +0200

    Create netflix_tv_long.sql
    feat: long-season TV stats (SQL)

---
+++ netflix_tv_long.sql
@@ -0,0 +1,20 @@
+SELECT
+    release_year,
+    COUNT(*) AS total_shows,
+    SUM(CASE
+            WHEN CAST(SUBSTR(duration, 1, INSTR(duration, ' ') - 1) AS INTEGER) > 2
+            THEN 1
+            ELSE 0
+        END) AS shows_gt_2_seasons,
+    (SUM(CASE
+             WHEN CAST(SUBSTR(duration, 1, INSTR(duration, ' ') - 1) AS INTEGER) > 2
+             THEN 1
+             ELSE 0
+         END) * 100.0 / COUNT(*)) AS percentage_gt_2_seasons
+FROM netflix
+WHERE
+    type = 'TV Show' AND release_year BETWEEN 2016 AND 2020
+GROUP BY
+    release_year
+ORDER BY
+    release_year;
